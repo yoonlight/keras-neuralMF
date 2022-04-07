c8d1b2906f9e27837e405dea0d72b613dec6c0a3
yoonlight <yoonlight12@gmail.com>
2022-04-07 22:39:18 +0900

    feat: add function to select implicit data

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,11 @@
-from movielens_dataset import load
+from dataset import movielens
 from src.model import NeuMF
 from keras import metrics, layers
 import numpy as np
 from sklearn.model_selection import train_test_split
 from common.plot import plot
 
-X, y, num_words_dict, _, _ = load.load_data()
+X, y, num_words_dict, _, _ = movielens.load_data(implicit=False)
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.1, random_state=42)
